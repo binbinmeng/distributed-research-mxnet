128aebd661df3606e0e17d5c8c649f3127f94283
Jiamin LI <jiaminli.icy@gmail.com>
2018-07-24 12:47:59 +0800

    check worker flow

--- python/mxnet/model.py
+++ python/mxnet/model.py
@@ -154,13 +154,12 @@ def _update_params_on_kvstore(param_arrays, grad_arrays, kvstore, param_names):
     """Perform update of param_arrays from grad_arrays on kvstore."""
     print('python->mxnet->model.py: line 155')
     print('TEST: param_arrays, grad_arrays')
-    print(param_arrays)
-    print(grad_arrays)
     for index, pair in enumerate(zip(param_arrays, grad_arrays)):
         arg_list, grad_list = pair
         if grad_list[0] is None:
             continue
         name = param_names[index]
+        print('kvstore key name%s'%name)
         # push gradient, priority is negative index
         kvstore.push(name, grad_list, priority=-index)
         # pull back the weights
